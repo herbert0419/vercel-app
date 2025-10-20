a85808b0eed81c954739f7abe8ffeaec23286902
herbert0419 <devshobhan4@gmail.com>
2025-10-20 18:58:19 +0530

    testing api

--- instant2.py
+++ instant2.py
@@ -12,7 +12,7 @@ You are on a website that has just been deployed to production for the first tim
 Please reply with an enthusiastic announcement to welcome visitors to the site, explaining that it is live on production for the first time!
 """
     messages = [{"role": "user", "content": message}]
-    response = client.chat.completions.create(model="gpt-5-nano", messages=messages)
+    response = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
     reply = response.choices[0].message.content.replace("\n", "<br/>")
     html = f"<html><head><title>Live in an Instant!</title></head><body><p>{reply}</p></body></html>"
     return html
